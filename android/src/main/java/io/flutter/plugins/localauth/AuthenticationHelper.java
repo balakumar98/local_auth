33dcd955406a40b2d989bd47ea7a83d87b088953
bk <balashanmugam2016@gmail.com>
2021-02-07 13:35:22 +0530

    check

--- android/src/main/java/io/flutter/plugins/localauth/AuthenticationHelper.java
+++ android/src/main/java/io/flutter/plugins/localauth/AuthenticationHelper.java
@@ -236,7 +236,7 @@ class AuthenticationHelper extends FingerprintManagerCompat.AuthenticationCallba
           }
         };
 
-    Button button = (Button) view.findViewById(R.id.button_send);
+    Button button = (Button) view.findViewById(R.id.button_cancel);
     button.setOnClickListener(new View.OnClickListener() {
         public void onClick(View v) {
             // Do something in response to button click
