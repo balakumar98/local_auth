0ad1182d6d79b5cf25ee993a3a31103b3c87a274
bk <balashanmugam2016@gmail.com>
2021-02-13 19:55:49 +0530

    check

--- android/src/main/java/io/flutter/plugins/localauth/AuthenticationHelper.java
+++ android/src/main/java/io/flutter/plugins/localauth/AuthenticationHelper.java
@@ -231,9 +231,9 @@ class AuthenticationHelper extends FingerprintManagerCompat.AuthenticationCallba
     // View clr = LayoutInflater.from(activity).inflate(R.values.colors, null, false);
     // int dark = LayoutInflater.from(activity).getResources().getColor(R.colors.dark_bg);
     // if(darTheme2) {
-    view.findViewById(R.id.main_layout).setBackgroundColor(696969);
+    // view.findViewById(R.id.main_layout).setBackgroundColor(696969);
     // }else {
-    //   view.findViewById(R.id.main_layout).setBackgroundColor(999999);
+    view.findViewById(R.id.main_layout).setBackgroundColor(999999);
     // }
     status.setText((String) call.argument("fingerprintHint"));
     Context context = new ContextThemeWrapper(activity, R.style.AlertDialogCustom);
